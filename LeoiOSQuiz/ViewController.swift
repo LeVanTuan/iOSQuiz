7a886d1fdbfb0153c532f5ee7a3b6ca6603e6fee
Anh Tuan <vndev.dn.2016@gmail.com>
2017-04-15 08:01:34 +0700

    Initial Commit

---
+++ LeoiOSQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeoiOSQuiz
+//
+//  Created by NeoLap on 4/15/17.
+//  Copyright © 2017 Leo LE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
